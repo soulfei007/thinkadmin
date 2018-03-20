847c903e43d42c61e38dacd73950223061d06c71
jin <jinfei@xiyouxiqj.com>
2018-03-20 18:49:23 +0800

    rbac

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -4,33 +4,20 @@ use controller\BasicAdmin;
 use think\Db;
 
 
-
 class User extends BasicAdmin{
 
 	public function add(){
-	   /*if( $this->request->isPost() ){
-          $data['username'] = trim(input('post.username'));
-          $data['email'] = trim(input('post.email'));
-          $data['salt'] = rand(100,9999);
-          $data['pass'] = trim(input('post.pass'));
-          $data['repass'] = trim(input('post.repass'));
-          $data['password'] = md5(trim(input('post.pass')).md5($data['salt']));
-          $validate = Loader::validate('User');
-          if(!$validate->check($data)){
-             //dump($validate->getError());
-             $this->error($validate->getError());
-          }else{
-          	$user = new User($data);
-          	$res = $user->allowField(true)->save(); 
-          	if($res){
-          	  $this->success('添加用户成功');
-          	}
-          }     
-	   }else{
-          return $this->fetch('');
-	   }*/
+       
 	   $this->table = 'xy_user';
-	   $this->_form();
+	   $this->valObj = 'User';
+	   $this->validate = true;
+       $data['username'] = trim(input('post.username'));
+       $data['email'] = trim(input('post.email'));
+       $data['salt'] = rand(100,9999);
+       $data['pass'] = trim(input('post.pass'));
+       $data['repass'] = trim(input('post.repass'));
+       $data['password'] = md5(trim(input('post.pass')).md5($data['salt']));
+	   $this->_form( $dbQuery=null, $tplFile='', $data );
 	   return $this->fetch('');
 	}
 
--- extend/controller/BasicAdmin.php
+++ extend/controller/BasicAdmin.php
@@ -8,29 +8,39 @@ class BasicAdmin extends Controller{
 
    //是否验证
    public $validate = false;
-
+   public $valObj;
    public $table ;
 
-   protected function _form( $dbQuery=null, $tplFile='', $data =[]){
+   protected function _form( $dbQuery=null, $tplFile='', $data =[] ){
       $db = ($dbQuery)?$dbQuery:Db::name($this->table);
       if( $this->request->isPost() ){
       	 if( $data ){
-            
-      	 }else{
-      	   $datas = $this->request->post();
-           $tableField = $db->getTableFields(['table'=>$this->table]);
-           foreach( $tableField as $k => $v ){ 
-              if( isset($datas[$v]) ){
-              	$allowDatas[$v] = $datas[$v];
-              }
-           }
+            if( $this->validate ){
+               if( $check = $this->_check($this->valObj,$data) === true ){
+               	 $result = $db->insert($data);
+               } 
+            }
+      	 }else {
+            $datas = $this->request->post();
+            $tableField = $db->getTableFields(['table'=>$this->table]);
+            foreach( $tableField as $k => $v ){ 
+               if( isset($datas[$v]) ){
+               	$allowDatas[$v] = $datas[$v];
+               }
+            }
+            $result = $db->insert($data);
       	 }
+      	 $result !== false ? $this->success('数据保存成功!',''):$this->error('数据保存失败!');	 
       }
    }
 
-   private function _check(){
-
-
+   private function _check( $valObj,$data ){
+      $validate = Loader::validate( $valObj );
+      if( $validate->check($data) ){
+         return true;
+      }else{
+      	return  $this->error($validate->getError());
+      }
 
    }
 
